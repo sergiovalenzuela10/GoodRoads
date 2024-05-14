fe7216db77c8964499ccc0d833048da83e65f5db
LORENA2412 <angielorenapuertacorredor@gmail.com>
2024-05-13 23:40:08 -0500

    Organizar layout

--- app/src/main/java/com/example/prueba1/MainActivity.kt
+++ app/src/main/java/com/example/prueba1/MainActivity.kt
@@ -2,20 +2,11 @@ package com.example.prueba1
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.prueba1.ui.theme.Prueba1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.inicio)
+        setContentView(R.layout.contrasena2)
     }
 }
 
