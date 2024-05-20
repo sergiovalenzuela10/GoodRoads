2fedc3464194d4528e50e3710853eb2b1d6162e8
JuliOrtiz09 <juliethingortiz95@gmail.com>
2024-05-19 20:01:39 -0500

    Se inicia codificación para maps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
+    implementation ("androidx.fragment:fragment-ktx:1.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/prueba1/reporte.kt
+++ app/src/main/java/com/example/prueba1/reporte.kt
@@ -1,14 +1,38 @@
 package com.example.prueba1
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.activity.ComponentActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.GoogleMap
 
+class reporte : ComponentActivity(), OnMapReadyCallback {
 
-class reporte : ComponentActivity() {
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_reporte)
+
+        val txtLatitud = findViewById<EditText>(R.id.txtLatitud)
+        val txtLongitud = findViewById<EditText>(R.id.txtLongitud)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.setOnMapClickListener { latLng ->
+            // Implementación del listener para clicks en el mapa
+        }
+        mMap.setOnMapLongClickListener { latLng ->
+            // Implementación del listener para clicks largos en el mapa
+        }
     }
 }
